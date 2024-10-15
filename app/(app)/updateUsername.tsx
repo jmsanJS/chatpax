b5bbe7afbff263f1b1afc4896a13973f1789388e
Juan Sanchez <juanboca90@gmail.com>
2024-10-15 15:54:51 +0200

    Update username screen

--- app/(app)/updateUsername.tsx
+++ app/(app)/updateUsername.tsx
@@ -1,22 +1,60 @@
-import { View, TextInput, StyleSheet, Text, Pressable } from "react-native";
-import React from "react";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Text,
+  Pressable,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
 import { useSession } from "@/context/context";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+import { auth, db } from "@/firebaseConfig";
+import { doc, updateDoc } from "firebase/firestore";
+import { useRouter } from "expo-router";
+import SettingsOptionsHeader from "@/components/SettingsOptionsHeader";
 
 export default function UpdateUsernameScreen() {
-  const { user } = useSession();
+  const [username, setUsername] = useState("");
+  const { user, setUser } = useSession();
+  const router = useRouter();
+
+  const handleUsernameUpdate = async () => {
+    if (user && auth.currentUser) {
+      try {
+        const userDocRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(userDocRef, {
+          username: username,
+        });
+        setUser({ ...user, username: username });
+        router.back();
+      } catch (error) {
+        const errorMsg = (error as Error).message;
+        Alert.alert("Error updating username", errorMsg);
+      }
+    } else {
+      Alert.alert("User not found");
+    }
+  };
 
   return (
     <View style={styles.container}>
+      <SettingsOptionsHeader user={user} router={router} />
       <View style={styles.formContainer}>
-        <Text>Your actual email:</Text>
-        <TextInput style={styles.input}></TextInput>
-        <Text>Type your new email:</Text>
-        <TextInput style={styles.input}></TextInput>
-        <Pressable style={styles.btn}>
+        <Text style={styles.title}>Update your username here</Text>
+        <Text style={styles.subtitle}>
+          Your actual username is "{user?.username}".
+        </Text>
+        <TextInput
+          style={styles.input}
+          placeholder="New username"
+          onChangeText={(value) => setUsername(value)}
+          value={username}
+        />
+        <Pressable style={styles.btn} onPress={handleUsernameUpdate}>
           <Text style={styles.btnText}>update</Text>
         </Pressable>
       </View>
@@ -27,18 +65,28 @@ export default function UpdateUsernameScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: -100,
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "#fff",
   },
   formContainer: {
     width: wp(80),
   },
+  title: {
+    fontSize: hp(3),
+    marginBottom: 30,
+  },
+  subtitle: {
+    fontSize: hp(2.2),
+    marginBottom: 20,
+  },
   input: {
     borderWidth: 1,
-    borderBlockColor: "magenta",
+    borderColor: "magenta",
     borderRadius: 10,
     padding: 10,
-    fontSize: hp(2.5)
+    fontSize: hp(2.5),
   },
   btn: {
     marginTop: 20,
@@ -47,11 +95,11 @@ const styles = StyleSheet.create({
     backgroundColor: "magenta",
   },
   btnText: {
-    fontSize: hp(2.5),
+    fontSize: hp(2.2),
     textAlign: "center",
     fontWeight: "bold",
     letterSpacing: 1,
     textTransform: "uppercase",
     color: "white",
-  }
+  },
 });
